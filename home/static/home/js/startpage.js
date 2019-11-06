9a01958e4481ef54f933d666e6579e3d6b19c95f
FellowshipOfThePing <jeremybinderdrummer@gmail.com>
2019-11-05 20:41:24 -0800

    Startpage redesign. Adjust general theme/colors (switched to MDB from basic boostrap). Create customPill class for Bottom row nav pills.

--- home/static/home/js/startpage.js
+++ home/static/home/js/startpage.js
@@ -1,6 +1,5 @@
 // ----------- HOMEPAGE JS ------------- //
 
-
 // HTML Content for blurbs
 var aboutText = "I just wanna help out!";
 var linksText = "Add something here later about links";
@@ -8,7 +7,8 @@ var contactText = "Contact stuff";
 var legalText = "The information contained on HTMLPrep.com and any publications or study materials created by the author of HTMLPrep.com is for informational purposes only. They are to be used to study and review for the ACT, SAT, or GRE. Exam content review, methods of study, tips and sample questions are only recommendations from the author, and reading any information on HTMLPrep.com and any publications or other study materials created by the author does not guarantee passing any of the aforementioned exams. The author has made reasonable efforts to provide current and accurate information. The author will not be held liable for any unintentional errors or omissions that may be found.";
 
 
-// Open Homepage
+
+// Open Page 2
 $("#start").on('click', function(event) {
     event.preventDefault();
     $("#jumbo1").fadeOut(400, function() {
@@ -18,15 +18,16 @@ $("#start").on('click', function(event) {
 });
 
 
-// Fade out blurb, change html, change active state, fade in
-$(".pill").on('click', function(event) {
+
+// Bottow Row Transition Animation
+$(".customPill").on('click', function(event) {
     event.preventDefault();
 
     // deactivate all pills
     deactivateAll();
 
     // activate selected pill
-    $(this).addClass("active");
+    $(this).addClass("current");
 
     // fade out title and text, change each mid fade
     var id = checkId($(this).attr("id"));
@@ -36,22 +37,24 @@ $(".pill").on('click', function(event) {
     $("#text").fadeOut(150, function() {
         $("#text").text(id[1]);
     });
-    // fade back in
 
+    // fade back in
     $("#title").fadeIn();
     $("#text").fadeIn();
 });
 
 
-// Shut off all menu pills before reactivating selected one
+
+// Deactivate All Pills
 function deactivateAll() {
-    $.each($(".pill"), function() {
-        $(this).removeClass("active");
+    $.each($(".customPill"), function() {
+        $(this).removeClass("current");
     });
 }
 
 
-// Return id of selected pill
+
+// Return id of Selected pill
 function checkId(id) {
     if(id === "about") {
         return ["About Us", aboutText];
